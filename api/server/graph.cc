5d24c6d7f2556098e59686e5d24ab1396ccf08ea
Matthias Gatto <matthias.gatto@outscale.com>
2017-08-21 13:59:11 +0200

    api/server: check OUTER_IPV4_CKSUM instead of TCP_TSO for offloading
    some nic handle PG_NIC_TX_OFFLOAD_TCP_TSO but doesn't handle
    inner packets checksuming, which doesn't work with vxlan.
    
    PG_NIC_TX_OFFLOAD_OUTER_IPV4_CKSUM allow to check that the nic
    handle inner packet offloading.
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- api/server/graph.cc
+++ api/server/graph.cc
@@ -170,7 +170,7 @@ bool Graph::Start(std::string dpdk_args) {
     }
     pg_nic_capabilities(nic_.get(), &useless, &nic_capa_tx);
     if (app::config.no_offload ||
-        !(nic_capa_tx & PG_NIC_TX_OFFLOAD_TCP_TSO)) {
+        !(nic_capa_tx & PG_NIC_TX_OFFLOAD_OUTER_IPV4_CKSUM)) {
         if (app::config.no_offload)
             app::log.Info("offloading manually desactivated");
         else
