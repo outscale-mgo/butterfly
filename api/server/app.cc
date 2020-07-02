417361f4ccf107f55d83454dc18c4016b92af0db
Matthias Gatto <matthias.gatto@outscale.com>
2020-07-02 17:16:51 +0200

    summer cleanup
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- api/server/app.cc
+++ api/server/app.cc
@@ -19,6 +19,10 @@ extern "C" {
 #include <syslog.h>
 #include <execinfo.h>
 #include <glib.h>
+
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+
 #include <packetgraph/packetgraph.h>
 }
 #include <iostream>
@@ -522,8 +526,58 @@ void app::DestroyCgroup() {
 
 #undef BASH
 
+
+int chk_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
+    switch (x.reply_type) {
+	case AMQP_RESPONSE_NORMAL:
+            return 0;
+
+	case AMQP_RESPONSE_NONE:
+            fprintf(stderr, "%s: missing RPC reply type!\n", context);
+            break;
+
+	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+            fprintf(stderr, "%s: %s\n", context, amqp_error_string2(x.library_error));
+            break;
+
+	case AMQP_RESPONSE_SERVER_EXCEPTION:
+            switch (x.reply.id) {
+		case AMQP_CONNECTION_CLOSE_METHOD: {
+                    amqp_connection_close_t *m =
+                            (amqp_connection_close_t *)x.reply.decoded;
+                    fprintf(stderr, "%s: server connection error %uh, message: %.*s\n",
+                            context, m->reply_code, (int)m->reply_text.len,
+                            (char *)m->reply_text.bytes);
+                    break;
+		}
+		case AMQP_CHANNEL_CLOSE_METHOD: {
+                    amqp_channel_close_t *m = (amqp_channel_close_t *)x.reply.decoded;
+                    fprintf(stderr, "%s: server channel error %uh, message: %.*s\n",
+                            context, m->reply_code, (int)m->reply_text.len,
+                            (char *)m->reply_text.bytes);
+                    break;
+		}
+		default:
+                    fprintf(stderr, "%s: unknown server error, method id 0x%08X\n",
+                            context, x.reply.id);
+                    break;
+            }
+            break;
+    }
+
+    return 1;
+}
+
 int
 main(int argc, char *argv[]) {
+    int ret  = 0;
+    std::string hostname;
+    int port;
+    int status;
+    amqp_bytes_t queuename;
+    char const *exchange;
+    char const *bindingkey;
+
     try {
         // Register signals
         app::SignalRegister();
@@ -551,18 +605,103 @@ main(int argc, char *argv[]) {
         }
         InitCgroup(POLL_THREAD_MULTIPLIER);
         // Prepare & run API server
-	while (1) {
-		LOG_DEBUG_("ZMQ received a message");
-		process(request);
-		LOG_DEBUG_("ZMQ send");
+	amqp_connection_state_t conn;
+	amqp_socket_t *socket = NULL;
+
+	conn = amqp_new_connection();
+
+#define die(...)				\
+	do {fprintf(stderr, __VA_ARGS__); ret = 1; goto out;} while (0)
+
+#define die_on_amqp_error(func, err_msg)				\
+	if ((ret = chk_on_amqp_error(func, err_msg)) != 0)              \
+            goto out;
+
+#define die_on_error(f, ...)                    \
+        if (f < 0)                              \
+            die(__VA_ARGS__);
+
+	socket = amqp_tcp_socket_new(conn);
+	if (!socket) {
+            die("creating TCP socket %s %d", hostname.c_str(), port);
 	}
+
+	printf("yay %s\n", app::config.api_endpoint.c_str());
+	status = amqp_socket_open(socket, hostname.c_str(), port);
+	if (status) {
+		die("opening TCP socket %s %d %s", hostname.c_str(), port, strerror(-status));
+	}
+
+	die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
+				     "guest", "guest"),
+			  "Logging in");
+	amqp_channel_open(conn, 1);
+	die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+
+	{
+		amqp_queue_declare_ok_t *r = amqp_queue_declare(
+			conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table);
+		die_on_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue");
+		queuename = amqp_bytes_malloc_dup(r->queue);
+		if (queuename.bytes == NULL) {
+			fprintf(stderr, "Out of memory while copying queue name");
+			return 1;
+		}
+	}
+
+	amqp_queue_bind(conn, 1, queuename, amqp_cstring_bytes(exchange),
+			amqp_cstring_bytes(bindingkey), amqp_empty_table);
+	die_on_amqp_error(amqp_get_rpc_reply(conn), "Binding queue");
+
+	amqp_basic_consume(conn, 1, queuename, amqp_empty_bytes, 0, 1, 0,
+			   amqp_empty_table);
+	die_on_amqp_error(amqp_get_rpc_reply(conn), "Consuming");
+	while (1) {
+		LOG_DEBUG_("RabbitMQ received a message");
+
+		amqp_rpc_reply_t res;
+		amqp_envelope_t envelope;
+
+		amqp_maybe_release_buffers(conn);
+
+		res = amqp_consume_message(conn, &envelope, NULL, 0);
+
+		if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+			break;
+		}
+
+		printf("Delivery %u, exchange %.*s routingkey %.*s\n",
+		       (unsigned)envelope.delivery_tag, (int)envelope.exchange.len,
+		       (char *)envelope.exchange.bytes, (int)envelope.routing_key.len,
+		       (char *)envelope.routing_key.bytes);
+
+		if (envelope.message.properties._flags & AMQP_BASIC_CONTENT_TYPE_FLAG) {
+			printf("Content-type: %.*s\n",
+			       (int)envelope.message.properties.content_type.len,
+			       (char *)envelope.message.properties.content_type.bytes);
+		}
+		printf("----\n");
+
+		amqp_destroy_envelope(&envelope);
+	}
+	amqp_bytes_free(queuename);
+
+	die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS),
+			  "Closing channel");
+	die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS),
+			  "Closing connection");
+	die_on_error(amqp_destroy_connection(conn), "Ending connection");
+
+	LOG_DEBUG_("RabbitMQ send");
     } catch (std::exception & e) {
         LOG_ERROR_("%s", e.what());
+        ret = 1;
     }
 
+out:
     // Ask graph to stop
     app::graph.Stop();
 
     app::log.Info("butterfly exit");
-    return 0;
+    return ret;
 }
